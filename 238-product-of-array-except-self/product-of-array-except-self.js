df4b11aa3c6a1c5e11aaf254de6b397b5d21833a
Fawas Ali <fawasalikariyeri@gmail.com>
2026-05-25 00:20:47 +0530

    Time: 2 ms (96.42%) | Memory: 70.2 MB (64.02%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -9,10 +9,11 @@ var productExceptSelf = function(nums) {
         answer[i] = left;
         left *= nums[i];
     }
+
     let right = 1;
     for(let i = nums.length - 1; i >= 0; i--) {
         answer[i] *= right;
-        right *= nums[i]
+        right *= nums[i];
     }
     return answer;
 };
